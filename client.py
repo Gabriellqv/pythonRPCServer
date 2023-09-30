a968d30025c7240e8fb1f53e31548cd67e262aa0
Gabriellqv <gabriellqv@gmail.com>
2023-09-30 17:44:44 -0300

    Update client and server formatting

--- client.py
+++ client.py
@@ -9,16 +9,19 @@ def menu():
     print("4 - Sair")
     print("\n")
 
+
 def main():
     try:
         # Conecta ao servidor RPC
-        conn = rpyc.connect('localhost', 18812)  # Substitua 'localhost' pelo endereço do servidor, se necessário
+        conn = rpyc.connect(
+            "localhost", 18812
+        )  # Substitua 'localhost' pelo endereço do servidor, se necessário
 
         while True:
-            menu() # Chama a função menu
+            menu()  # Chama a função menu
             choice = input("Digite o número da opção desejada: ")
 
-            if choice == '1':
+            if choice == "1":
                 # Chama o método calculate_imc do servidor
                 print("----------------------------------")
                 print("Calculadora de IMC")
@@ -32,33 +35,17 @@ def main():
                     print("Erro: Insira valores numéricos válidos para peso e altura.")
                     continue  # Volte ao menu
 
-                imc = conn.root.exposed_calculate_imc(weight, height)
+                result_imc = conn.root.exposed_calculate_imc(weight, height)
+                imc, estate = result_imc
                 print("----------------------------------")
                 print("Vamos calcular seu IMC")
                 print("----------------------------------")
-
                 # Apresenta o resultado do IMC
                 print(f"Seu IMC é: {imc:.2f}")
+                # Apresenta o resultado do estado de saude do usuario
+                print(estate + "\n")
 
-                # Com o IMC calculado, esta parte classifica e apresenta o estado de saúde do usuário.
-                if imc < 16:
-                    print("Seu estado é de Magreza grave\nProcure um médico ou nutricionista.\n")
-                elif imc < 17:
-                    print("Seu estado é de Magreza moderada\nPrecisa rever sua alimentação.\n")
-                elif imc < 18.5:
-                    print("Seu estado é de Magreza leve\nPrecisa rever sua alimentação.\n")
-                elif imc < 25:
-                    print("Você está Saudável.")
-                elif imc < 30:
-                    print("Seu estado é de Sobrepeso\nPrecisa rever sua alimentação.\n")
-                elif imc < 35:
-                    print("Seu estado é de Obesidade Grau I\nProcure um médico ou nutricionista.\n")
-                elif imc < 40:
-                    print("Seu estado é de Obesidade Grau II (severa)\nProcure um médico ou nutricionista.\n")
-                else:
-                    print("Seu estado é de Obesidade Grau III (mórbida)\nProcure um médico ou nutricionista.\n")
-
-            elif choice == '2':
+            elif choice == "2":
                 # Chama o método calculate_quadraticEquation do servidor
                 print("----------------------------------")
                 print("Calcular equação de segundo grau")
@@ -66,20 +53,35 @@ def main():
 
                 # Solicitar os coeficientes da equação de segundo grau ao usuário (ax^2 + bx + c = 0)
                 try:
-                    a = float(input("Digite o valor do coeficiente 'a' (não pode ser zero): "))
+                    a = float(
+                        input("Digite o valor do coeficiente 'a' (não pode ser zero): ")
+                    )
                     while a == 0:
-                        print("***********************************************************")
-                        print("O coeficiente 'a' não pode ser zero. Tente um valor válido.")
-                        print("***********************************************************")
-                        a = float(input("Digite o valor do coeficiente 'a' (não pode ser zero): "))
+                        print(
+                            "***********************************************************"
+                        )
+                        print(
+                            "O coeficiente 'a' não pode ser zero. Tente um valor válido."
+                        )
+                        print(
+                            "***********************************************************"
+                        )
+                        a = float(
+                            input(
+                                "Digite o valor do coeficiente 'a' (não pode ser zero): "
+                            )
+                        )
                     b = float(input("Digite o valor do coeficiente 'b': "))
                     c = float(input("Digite o valor do coeficiente 'c': "))
                 except ValueError:
-                    print("Erro: Insira valores numéricos válidos para os coeficientes.")
+                    print(
+                        "Erro: Insira valores numéricos válidos para os coeficientes."
+                    )
                     continue  # Volte ao menu
 
                 print("----------------------------------")
 
+                # Apresenta o resultado da equacao
                 result = conn.root.exposed_calculate_quadraticEquation(a, b, c)
                 if isinstance(result, tuple):
                     print(f"As raízes são x1 = {result[0]} e x2 = {result[1]}\n")
@@ -89,9 +91,11 @@ def main():
                     else:
                         real_part = result[0].real
                         imaginary_part = result[0].imag
-                        print(f"As raízes são complexas: x1 = {real_part} + {imaginary_part}i e x2 = {real_part} - {imaginary_part}i\n")
+                        print(
+                            f"As raízes são complexas: x1 = {real_part} + {imaginary_part}i e x2 = {real_part} - {imaginary_part}i\n"
+                        )
 
-            elif choice == '3':
+            elif choice == "3":
                 # Chama o método check_palindrome do servidor
                 print("----------------------------------")
                 print("Verificador de palíndromo")
@@ -102,11 +106,11 @@ def main():
                 is_palindrome = conn.root.exposed_check_palindrome(word)
                 # Verifica se a palavra é igual à sua versão invertida.
                 if is_palindrome:
-                    print(f'{word} é um palíndromo.\n')
+                    print(f"{word} é um palíndromo.\n")
                 else:
-                    print(f'{word} não é um palíndromo.\n')
+                    print(f"{word} não é um palíndromo.\n")
 
-            elif choice == '4':
+            elif choice == "4":
                 print("Saindo do cliente.\n")
                 break  # Sair do loop e encerrar o programa
 
@@ -114,10 +118,13 @@ def main():
                 print("Opção inválida. Escolha uma opção válida.")
 
     except ConnectionRefusedError:
-        print("Erro: Não foi possível conectar ao servidor. Verifique se o servidor está em execução.")
+        print(
+            "Erro: Não foi possível conectar ao servidor. Verifique se o servidor está em execução."
+        )
     finally:
         if conn:
             conn.close()
+
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import rpyc  # Biblioteca para Python RPC
 from rpyc.utils.server import ThreadedServer
 
+
 class MyService(rpyc.Service):
     def on_connect(self, conn):
         pass
@@ -11,26 +12,46 @@ class MyService(rpyc.Service):
 
     def exposed_calculate_imc(self, weight, height):
         # Calcula o IMC e retorna o resultado.
-        imc = weight / (height ** 2)
-        return imc
+        imc = weight / (height**2)
+        # Com o IMC calculado, esta parte classifica e apresenta o estado de saúde do usuário.
+        if imc < 16:
+            estate = "Magreza grave. Procure um médico ou nutricionista."
+        elif imc < 17:
+            estate = "Seu estado é de Magreza moderada Precisa rever sua alimentação."
+        elif imc < 18.5:
+            estate = "Seu estado é de Magreza leve Precisa rever sua alimentação."
+        elif imc < 25:
+            estate = "Você está Saudável."
+        elif imc < 30:
+            estate = "Seu estado é de Sobrepeso Precisa rever sua alimentação."
+        elif imc < 35:
+            estate = (
+                "Seu estado é de Obesidade Grau I Procure um médico ou nutricionista."
+            )
+        elif imc < 40:
+            estate = "Seu estado é de Obesidade Grau II (severa) Procure um médico ou nutricionista."
+        else:
+            estate = "Seu estado é de Obesidade Grau III (mórbida) Procure um médico ou nutricionista."
+
+        return imc, estate
 
     def exposed_calculate_quadraticEquation(self, a, b, c):
         import math
 
         # Verifica as condicoes e calculas as raizes
-        discriminant = b**2 - 4*a*c
+        discriminant = b**2 - 4 * a * c
         if discriminant > 0:
-            x1 = (-b + math.sqrt(discriminant)) / (2*a)
-            x2 = (-b - math.sqrt(discriminant)) / (2*a)
+            x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+            x2 = (-b - math.sqrt(discriminant)) / (2 * a)
             return x1, x2
         elif discriminant == 0:
             # Raiz real única (raízes iguais)
-            x1 = -b / (2*a)
+            x1 = -b / (2 * a)
             return x1
         else:
             # Não há raízes reais (raízes complexas)
-            real_part = -b / (2*a)
-            imaginary_part = math.sqrt(-discriminant) / (2*a)
+            real_part = -b / (2 * a)
+            imaginary_part = math.sqrt(-discriminant) / (2 * a)
             return real_part + imaginary_part * 1j, real_part - imaginary_part * 1j
 
     def exposed_check_palindrome(self, word):
@@ -39,13 +60,14 @@ class MyService(rpyc.Service):
         word = word.lower().replace(" ", "")
         return word == word[::-1]
 
+
 # Verifica se o script está sendo executado como um programa principal
 if __name__ == "__main__":
     try:
-        server = ThreadedServer(MyService, hostname='0.0.0.0', port=18812)
-        print('Servidor online')
+        server = ThreadedServer(MyService, hostname="0.0.0.0", port=18812)
+        print("Servidor online")
         # Inicia o servidor, permitindo que ele comece a aceitar conexões de clientes
         server.start()
         # Captura uma exceção de interrupção do teclado para poder encerrar o servidor.
     except KeyboardInterrupt:
-        print('Servidor encerrado')
+        print("Servidor encerrado")
